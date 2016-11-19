4b5d2e7400c479e4dcc8bc47ac66a6a447554e99
QuHaofeng <mr_qhf@qq.com>
2016-11-19 20:30:46 +0800

    connect to firebase

--- app/src/main/java/cn/studyjams/s1/sj19/quhaofeng/MainActivity.java
+++ app/src/main/java/cn/studyjams/s1/sj19/quhaofeng/MainActivity.java
@@ -3,12 +3,20 @@ package cn.studyjams.s1.sj19.quhaofeng;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.analytics.FirebaseAnalytics;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "0");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Badminton");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 
 }
